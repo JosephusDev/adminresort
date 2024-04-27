28bc88ee1aba880b57de2d4e9aabfbc91ba123f2
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2024-04-27 09:31:12 -0300

    Update Login.tsx

--- src/shared/components/login/Login.tsx
+++ src/shared/components/login/Login.tsx
@@ -89,7 +89,7 @@ const Login: React.FC<{ children: React.ReactNode }> = ({ children }) => {
                     variant="contained"
                     color="primary"
                     disableElevation
-                    startIcon={<Icon>login</Icon>}
+                    startIcon={carregando ? <Carregando/> : <Icon>login</Icon>}
                     onClick={fazerLogin}
                     style={{ width: isSmallScreen ? "40%" : isMediumScreen ? "60%" : "30%" }}
                 >
